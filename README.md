e0bb484af4dcb24a027cad74eb953c8799c709d1
Ocean Deep <cloud@oceandeep.com.br>
2020-11-18 22:40:05 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,104 @@
+# Sobre
+
+ORCL é uma alternativa simples de executar comandos simples dentro de um banco database Oracle 12x.
+
+# Dependências
+
+- É necessário que tenha instalado o cliente SQLPlus correspondente ao seu banco de dados.
+
+# Instalação
+
+- Execute o comando <b>npm install --save sm-orcl</b> para obter o pacote.
+
+# Usando
+
+- Em seu arquivo realize o import do modulo.
+
+```javascript
+  const ORACLE = require('sm-orcl')
+  const orcl = new ORACLE({
+    ip_address: '127.0.0.1',
+    username: <username>,
+    passowrd: <password>,
+    service_name: <servicename>,
+    port: <port> //Default is 1521
+  })
+```
+# Comandos
+
+- Exemplo de insert
+
+```javascript
+...
+  const orcl = new Oracle(dbora.auth)
+  let user = {
+    name: 'Fulano Sauro',
+    idade: 23,
+    sexo: 'masculino'
+  }
+  orcl.insert({table: 'ex_user', data: user}).then((res)=>{
+    console.log(data)
+  }).catch(err => console.error(err))
+...
+```
+
+- Exemplo de removeById
+
+```javascript
+  ...
+    const orcl = new Oracle(dbora.auth)
+    orcl.removeById({table: 'ex_user', id: 1}).then((res)=>{
+      console.log(data)
+    }).catch(err => console.error(err))
+  ...
+```
+
+- Exemplo de delete com/sem where
+- Se informar que **deleteAll**:**false** vai respeitar a regra do where se estiver como **deleteAll**:**true** ele irá ignorar o where.
+
+```javascript
+  ...
+    const orcl = new Oracle(dbora.auth)
+    orcl.remove({table: 'ex_user', deleteAll: false, where: { id: 1, email_address: "fulano@ciclano.me" }}).then((res)=>{
+      console.log(data)
+    }).catch(err => console.error(err))
+  ...
+```
+
+- Exemplo de update com/sem where
+- Se informar que **whereAll**:**false** vai respeitar a regra do where se estiver como **whereAll**:**true** ele irá ignorar o where.
+
+```javascript
+  ...
+    const orcl = new Oracle(dbora.auth)
+    orcl.remove({table: 'ex_user', whereAll: false, where: { id: 1, email_address: "fulano@ciclano.me" }}).then((res)=>{
+      console.log(data)
+    }).catch(err => console.error(err))
+  ...
+```
+
+- Exemplo de query native
+
+```javascript
+  ...
+    const orcl = new Oracle(dbora.auth)
+
+    let user = {
+      name: 'Fulano Sauro',
+      idade: 23,
+      sexo: 'masculino'
+    }
+
+    orcl.query(`
+     inset into ex_user ( name, idade, sexo ) values ( '${user.name}', '${user.idade}', '${user.sexo}' );
+     exec EX_CREATEUSER( 
+       name => ${user.name},
+       idade => ${user.idade},
+       sexo => ${user.sexo}
+     );
+     select name from ex_user;
+    `).then((res)=>{
+      console.log(data)
+    }).catch(err => console.error(err))
+  ...
+```
---
+++ sm-orcl.js
@@ -0,0 +1,107 @@
+const util = require('util');
+const execPromissse = util.promisify(require('child_process').exec)
+
+var error = ""
+var data = ""
+
+async function exec(command) {
+  const { error, stdout, stderr } = await execPromissse(command)
+  return { error, stdout, stderr }
+}
+
+class ORACLE {
+  constructor({ username: username, passowrd: passowrd, ip_address: ip_address, port: port, service_name: service_name }) {
+    this.username = username, this.passowrd = passowrd, this.ip_address = ip_address, this.port = port ?? 1521, this.service_name = service_name
+  }
+
+  tns_connect() {
+    return `${this.username}/${this.passowrd}@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=${this.ip_address})(PORT=${this.port})))(CONNECT_DATA=(SERVICE_NAME=${this.service_name})))`
+  }
+
+  search_ora(output) {
+    for (let i of output) {
+      for (let x of i) {
+        if (x.search('ORA-') !== -1) {
+          return {
+            status: 500,
+            data: [],
+            error: x
+          }
+        }
+      }
+    }
+  }
+
+  async sqlplus(sql) {
+    try {
+      const response = await (await exec(`export NLS_LANG=AMERICAN_AMERICA.UTF8 \n sqlplus -s "${this.tns_connect()}" << EOF \n set pages 0 \n ${sql} \nEOF`)).stdout
+      data = response.split("\n").map(arry => { return arry.split("\t") }).filter(el => { return el != '' })
+      //console.log(data)
+      if (this.search_ora(data)) return this.search_ora(data)
+      return { status: 200, data: data[0], error: error }
+    } catch (error) {
+      console.log(error)
+      return { status: 500, data: data, error: "Internal Server Error" }
+    }
+  }
+
+  async insert({ table, data }) {
+    var patchDataValues = `\'${Object.values(data).join(',').replace(/,/g, '\',\'')}\'`
+    let res = await this.sqlplus(`insert into ${table} ( ${Object.keys(data)} ) values ( ${patchDataValues} );`)
+    return res
+  }
+
+  objToArrayWithComparisionOfAny(data) {
+    let obj = []
+    let objDataKeys = Object.keys(data)
+    let objDataValues = Object.values(data)
+    let idx = 0
+    for (const ky of objDataKeys) {
+      obj.push(`${ky} = \'${objDataValues[idx]}\'`)
+      idx++
+    }
+    return obj
+  }
+
+  async update({ table, data, whereAll, where }) {
+    var isExistWhere = ";"
+    let objUpdate = this.objToArrayWithComparisionOfAny(data)
+    var objWhere = []
+    if (whereAll) {
+      isExistWhere = ";"
+    } else {
+      objWhere = this.objToArrayWithComparisionOfAny(where)
+      isExistWhere = " where " + objWhere.join(',').replace(/,/g, ' and ') + ";"
+    }
+    let res = await this.sqlplus(`update ${table} set ${objUpdate} ${isExistWhere}`)
+    return res
+  }
+
+  async remove({ table, deleteAll, where }) {
+    var isExistWhere = ";"
+    var objWhere = []
+    if (deleteAll) {
+      isExistWhere = ";"
+    } else {
+      objWhere = this.objToArrayWithComparisionOfAny(where)
+      isExistWhere = " where " + objWhere.join(',').replace(/,/g, ' and ') + ";"
+    }
+    let res = await this.sqlplus('delete ' + table + isExistWhere)
+    return res
+  }
+
+  async removeById({ table, id }) {
+    let res = await this.sqlplus(`delete ${table} where id = ${id};`)
+    return res;
+  }
+
+  async query(query) {
+    var scape
+    if (query.search('begin') !== -1) scape = "/"
+    let res = await this.sqlplus(`${query} \n ${scape}`)
+    return res
+  }
+
+}
+
+module.exports = ORACLE
